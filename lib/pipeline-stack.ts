7b3c758257afd0d5752b54837ec11f3a81e4162d
Brian Farias <92887307+BrianBFarias@users.noreply.github.com>
2025-01-16 15:40:15 -0500

    remove type module

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -3,22 +3,76 @@ import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ManualApprovalStep, ShellStep } from 'aws-cdk-lib/pipelines';
 import { PipelineAppStage } from './app-stage';
 import { PipelineCentralStage } from './central-stage';
-import { AWS_BETA_ACCOUNT, AWS_CENTRAL_ACCOUNT, AWS_PROD_ACCOUNT, AWS_REGION } from '../global/constants';
+import { 
+  AWS_BETA_ACCOUNT, 
+  AWS_CENTRAL_ACCOUNT, 
+  AWS_PROD_ACCOUNT, 
+  AWS_REGION, 
+  GITHUB_ARN 
+} from '../global/constants';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class PipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // modify permissions for the pipeline to use codeconnection
+    const pipelineRole = new iam.Role(this, 'PipelineRole', {
+      assumedBy: new iam.ServicePrincipal('codepipeline.amazonaws.com'),
+    });
+
+    pipelineRole.addToPolicy(new iam.PolicyStatement({
+      actions: [
+        "codeconnections:GetConnection",
+        "codeconnections:UseConnection",
+        "codeconnections:GetRepositorySyncStatus",
+        "codeconnections:GetSyncConfiguration",
+        "codeconnections:GetConnectionToken",
+        "codeconnections:GetHost",
+        "codeconnections:StartOAuthHandshake",
+        "codeconnections:PassRepository",
+        "codeconnections:UseConnection",
+        "codebuild:BatchPutCodeCoverages",
+				"codebuild:BatchPutTestCases",
+				"codebuild:CreateReport",
+				"codebuild:CreateReportGroup",
+				"codebuild:UpdateReport",
+        "s3:Abort*",
+				"s3:DeleteObject*",
+				"s3:GetBucket*",
+				"s3:GetObject*",
+				"s3:List*",
+				"s3:PutObject",
+				"s3:PutObjectLegalHold",
+				"s3:PutObjectRetention",
+				"s3:PutObjectTagging",
+				"s3:PutObjectVersionTagging",
+        "kms:Decrypt",
+				"kms:DescribeKey",
+				"kms:Encrypt",
+				"kms:GenerateDataKey*",
+				"kms:ReEncrypt*",
+        "logs:CreateLogGroup",
+				"logs:CreateLogStream",
+				"logs:PutLogEvents"
+      ],
+      resources: [
+        'arn:aws:us-east-1:724772076019:*',
+      ],
+    }));
+    
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyRewards',
       crossAccountKeys: true, 
       selfMutation:true,
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('My-Rewards/pipeline', 'main', {
-          authentication: cdk.SecretValue.secretsManager('github-token'),
+        input: CodePipelineSource.connection('My-Rewards/pipeline', 'main', {
+          connectionArn: GITHUB_ARN,
+          triggerOnPush: true,
         }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
-      })
+      }),
+      role:pipelineRole
     });
 
     pipeline.addStage(new PipelineCentralStage(this, "central", {
