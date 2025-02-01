de7e07714d248c2869840d8cbb60b8772c3eab7c
Brian Farias <92887307+BrianBFarias@users.noreply.github.com>
2025-02-01 10:32:31 -0500

    email sending

--- lambda/user/createUserBusiness.js
+++ lambda/user/createUserBusiness.js
@@ -1,13 +1,16 @@
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
 const { DynamoDBDocumentClient, PutCommand } = require('@aws-sdk/lib-dynamodb');
+const { SESClient, SendEmailCommand } = require('@aws-sdk/client-ses');
 
 const client = new DynamoDBClient({});
 const dynamoDb = DynamoDBDocumentClient.from(client);
+const ses = new SESClient({ region: 'us-east-1' }); 
 
 exports.handler = async (event) => {
   const tableName = process.env.TABLE;
   const role = process.env.ROLE;
-  
+  const emailSender = process.env.EMAIL_SENDER;
+
     try {
         const { request: { userAttributes } } = event;
         
@@ -38,14 +41,24 @@ exports.handler = async (event) => {
         };
 
         const params = {
-        TableName: tableName,
-        Item: userData,
-        ConditionExpression: 'attribute_not_exists(id)'
+            TableName: tableName,
+            Item: userData,
+            ConditionExpression: 'attribute_not_exists(id)'
+        };
+
+        const emailParams = {
+            Source: emailSender,
+            Destination: { ToAddresses: [userAttributes.email] },
+            Message: {
+                Subject: { Data: 'Welcome To MyRewards!' },
+                Body: { Text: { Data: 'Setup Your account and link with awuare if you havent! We have a feeling your gonna like it here.' } },
+            },
         };
 
         await dynamoDb.send(new PutCommand(params));
-        console.log('Successfully saved user:', userAttributes.sub);
-        
+
+        await ses.send(new SendEmailCommand(emailParams));
+
         return event;
         
     } catch (error) {
