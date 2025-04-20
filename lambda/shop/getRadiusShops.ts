1ea9469311e32799f6f52605bfa232617906e850
Landon Zigler <landonzigler@gmail.com>
2025-04-19 20:51:16 -0400

    add long and lat

--- lambda/shop/getRadiusShops.ts
+++ lambda/shop/getRadiusShops.ts
@@ -87,7 +87,7 @@ export const handler = async (event: APIGatewayProxyEvent) => {
             new GetCommand({
               TableName: shopTable,
               Key: { id: shopDetails.shop_id },
-              ProjectionExpression: "#loc, shop_hours",
+              ProjectionExpression: "#loc, shop_hours, longitude, latitude",
               ExpressionAttributeNames: {
                 "#loc": "location",
               },
@@ -125,6 +125,8 @@ export const handler = async (event: APIGatewayProxyEvent) => {
             shop_id: shopDetails.shop_id,
             organization_id: shopDetails.org_id,
             preview: org.images?.banner?.url || "",
+            latitude: shop.latitude,
+            longitude: shop.longitude,
             name: org.name,
             distance: miles,
             favorite,
